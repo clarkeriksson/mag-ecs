26e89f77274d39a9ac75e4bdddeeb52578fdffa6
Clark Eriksson <erikssonclarkw@gmail.com>
2025-07-06 16:31:08 -0400

    Trying to fix global declarations. Again.

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+/// <reference path="./globals.d.ts" />
+export * from './core/component';
+export * from './core/system';
+export * from './core/world';
+export * from './core/mag-event';
+export * from './core/query-definition';
